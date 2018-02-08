44fc2aa73c2b1324a87d817b3a29c8ce6bd09e18
DESKTOP-K9BKKJ0\ghwls <ghwls328@gmail.com>
2018-02-09 02:26:19 +0900

    유니티 5로 만드는 3D/2D 스마트폰 게임 개발 챕터5

---
+++ Assets/Script/GameControll.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameControll : MonoBehaviour {
+
+    public NejikoController nejiko;
+    public Text scoreLabel;
+    public LifePanel lifePanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    int CalcScore()
+    {
+        return (int)nejiko.transform.position.z;
+    }
+	// Update is called once per frame
+	void Update () {
+        int score = CalcScore();
+        scoreLabel.text = "Score : " + score + "m";
+        lifePanel.UpdateLife(nejiko.Life());
+
+        if (nejiko.Life() <= 0)
+        {
+            enabled = false;
+
+            if (PlayerPrefs.GetInt("HighScore") < score)
+            {
+                PlayerPrefs.SetInt("HighScore", score);
+            }
+            Invoke("ReturnToTitle", 2.0f);
+        }
+	}
+    void ReturnToTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
---
+++ Assets/Script/LifePanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifePanel : MonoBehaviour {
+
+    public GameObject[] Icons;
+    
+    public void UpdateLife(int life)
+    {
+        for(int i = 0; i <Icons.Length; i++)
+        {
+            if (i < life) Icons[i].SetActive(true);
+            else Icons[i].SetActive(false);
+        }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
